796ed39ece7a107682d210ff62aac44a832a4c86
AIPhD <sbilaj@gmail.com>
2025-09-30 01:57:20 +0200

    reward out

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import evaluation as e
 from cartpole_project import training_cartpole as tc
 from maze_project import training_maze_old as tm
 from maze_project import rl_maze as rlm
+from atari_project import rl_atari as rla
 from atari_project import training_atari_old as ta
 
 
@@ -25,8 +26,11 @@ def main():
     # ddqn_sim_maze = rlm.MazeDDQNLearning('mazenavigating')
     # ddqn_sim_maze.train_agent()
 
-    a2c_sim_maze = rlm.MazeA2CLearning('mazenavigating', n_envs=16, transfer=None)
-    a2c_sim_maze.train_agent()
+    # a2c_sim_maze = rlm.MazeA2CLearning('mazenavigating', n_envs=16, transfer=None)
+    # a2c_sim_maze.train_agent()
+
+    a2c_sim_atari = rla.AtariA2CLearning('Centipede', n_envs=n_envs, transfer=None)
+    a2c_sim_atari.train_agent()
 
     # ta.train_a2c_baseline('SpaceInvaders')
     # ta.train_network()
